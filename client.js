90ca77ce9f714d336a0165fb18d3a898ffcc3d0b
Dimitri Rosenberg <dimitri@range-app.com>
2016-03-11 12:39:57 +0300

    feat(client): send a new message

--- client.js
+++ client.js
@@ -1,10 +1,124 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-class Main extends React.Component {
-  render() {
-    return <div>Hello, Theron!</div>;
+const DEFAULT_HEADERS = { 'Accept': 'application/json', 'Content-Type': 'application/json' }
+
+class Api {
+  fetchMessages() {
+    return fetch('/messages').then(Api.checkStatus).then(Api.parse);
+  }
+
+  createMessage(payload) {
+    return fetch('/messages', { method: 'POST', headers: DEFAULT_HEADERS, body: Api.stringify(payload) }).then(Api.checkStatus).then(Api.parse);
+  }
+
+  static async checkStatus(res) {
+    if (res.status >= 200 && res.status < 300) {
+      return res;
+    }
+
+    throw new Error(res.statusText);
+  }
+
+  static parse(res) {
+    return res.json();
+  }
+
+  static stringify(body) {
+    return JSON.stringify(body);
   }
 }
 
-ReactDOM.render(<Main />, document.getElementById('main'));
+class Message extends React.Component {
+  render() {
+    const { message: { user, message, created_at } } = this.props;
+
+    return (
+      <li>
+        <div className="meta">
+          <span className="user">{user}</span>
+          <span className="time">{created_at}</span>
+        </div>
+
+        <div className="message">{message}</div>
+      </li>
+    );
+  }
+}
+
+class MessageForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { user: '', message: '' };
+  }
+
+  onUserChange(event) {
+    this.setState({ user: event.target.value });
+  }
+
+  async sendMessage(event) {
+    this.setState({ message: this._input.value });
+
+    if (event.which === 13) {
+      const { api } = this.props;
+
+      try {
+        await api.createMessage(this.state);
+        this._input.value = '';
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+  }
+
+  render() {
+    return (
+      <footer className="form">
+        <input type="text" className="user" placeholder="Your name" onChange={this.onUserChange.bind(this)} />
+        <input type="text" className="message" placeholder="Type your message here..." ref={input => this._input = input} onKeyUp={this.sendMessage.bind(this)} />
+      </footer>
+    );
+  }
+}
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { messages: [] };
+  }
+
+  componentDidMount() {
+    this.fetchMessages();
+    setInterval(this.fetchMessages.bind(this), 2000);
+  }
+
+  async fetchMessages() {
+    const { api } = this.props;
+
+    try {
+      const messages = await api.fetchMessages();
+      this.setState({ messages });
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    const { messages } = this.state;
+    const { api } = this.props;
+
+    return (
+      <main>
+        <section className="history">
+          <ul>
+            {messages.map(message => <Message message={message} key={message.id} />)}
+          </ul>
+        </section>
+
+        <MessageForm api={api} />
+      </main>
+    );
+  }
+}
+
+ReactDOM.render(<Main api={new Api()} />, document.getElementById('main'));
--- server.js
+++ server.js
@@ -16,8 +16,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Serve assets
 
 app.use('/jspm_packages', express.static('./jspm_packages'));
-
 app.use('/', express.static('./'));
+
+// Api
+
+const now = () => new Date().toISOString();
+
+app.get('/messages', ({ body }, res) => {
+  pg('messages').orderBy('created_at').then(rows => res.json(rows));
+});
+
+app.post('/messages', ({ body }, res) => {
+  pg('messages').insert(Object.assign({}, body, { created_at: now(), updated_at: now() })).then(row => res.json(row));
+});
 
 // Start server
 
