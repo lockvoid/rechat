aa26426019bbc517e76227b339376946e96370f3
Dimitri Rosenberg <dimitri@range-app.com>
2016-04-07 18:47:32 +0300

    feat(db): rename POSTGRES_URL to DATABASE_URL

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     client: 'postgresql',
 
-    connection: process.env['POSTGRES_URL'],
+    connection: process.env['DATABASE_URL'],
 
     pool: {
       min: 2, max: 10
@@ -18,7 +18,7 @@ module.exports = {
   production: {
     client: 'postgresql',
 
-    connection: process.env['POSTGRES_URL'],
+    connection: process.env['DATABASE_URL'],
 
     pool: {
       min: 2, max: 10
